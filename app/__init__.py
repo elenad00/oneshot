66070c6ae681a486d9e18729a6919aa2133714b8
elena <elenajeanday@gmail.com>
2024-10-27 16:53:52 +0000

    added types

---
+++ app/__init__.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index() -> str:
+    return render_template('home.html')
+
+@app.route('/<username>')
+def profile(username) -> str:
+    return render_template('profile.html')
+
+@app.route('/write')
+def books() -> str:
+    return render_template('write.html')
+
+
+# basic log in sign up functions
+@app.route('/login')
+def login() -> str:
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup() -> str:
+    return render_template('signup.html')
+
+@app.route('/error')
+def error() -> str:
+  return render_template('blank.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
